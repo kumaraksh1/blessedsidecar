26e2372b22d1b11d74cffc9f3f6d6332133fcecc
Akshay Kumar <112485097+kumaraksh1@users.noreply.github.com>
2025-07-29 12:40:36 +0530

    Update app.py

--- hello-app/app.py
+++ hello-app/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/hello', methods=['GET'])
 def hello():
-    return jsonify({"message": "Hello from Hello App!, testing sidecar gha with blessed scenario #hopefully final testing2"})
+    return jsonify({"message": "Hello from Hello App!, testing sidecar gha with blessed scenario #hopefully final testing before release "})
 
 
 @app.route('/read-file', methods=['GET'])
